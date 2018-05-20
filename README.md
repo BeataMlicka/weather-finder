693ddfe7f9d1f43b2b856eee71c3709df1e1cb2e
Beata <mlicka.beata@gmail.com>
2018-05-20 23:32:57 +0200

    Weather finder - finished project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # weather-finder
-React app to check the weather
+
+A simple react app uses the node server to check the weather in a given place.
+
+===== package installation and run app =====
+
+1. npm install
+2. npm start
---
+++ src/components/Titles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Titles = () => (
+
+    <
+    div className = "header" >
+    <
+    h1 > Weather Finder < /h1>  <
+    p > Where would you like to check the weather ? < /p> < /
+    div >
+
+);
+
+export default Titles;
